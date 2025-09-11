a8e899665b61c9abf6a160631ab68218da8e93df
Endgineer <bm.alkhudairy@hotmail.com>
2025-09-11 15:47:42 -0400

    Removed unused nightmare items and added nightmare fuel texture

--- src/main/java/croissantnova/sanitydim/item/NightmareFuelItem.java
+++ src/main/java/croissantnova/sanitydim/item/NightmareFuelItem.java
@@ -1,32 +1,9 @@
 package croissantnova.sanitydim.item;
 
-import croissantnova.sanitydim.client.ItemTooltipHelper;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
-
-import javax.annotation.Nullable;
-import java.util.List;
 
 public class NightmareFuelItem extends Item {
-
     public NightmareFuelItem() {
-        super(new Properties().stacksTo(16));
-    }
-
-    @Override
-    public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, @NotNull List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced)
-    {
-        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
-        ItemTooltipHelper.showTooltipOnShift(pTooltipComponents, "nightmare_fuel");
-    }
-
-    @Override
-    public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
-        return 200 * 64;
+        super(new Properties());
     }
 }
